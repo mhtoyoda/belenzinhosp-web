3fac8b927dec16add28a15be90f665814e7977d1
Marcelo Toyoda <marcelo.toyoda@koin.com.br>
2019-01-01 22:30:05 -0200

    Criacao de formulário de contatos

--- src/app/fale-conosco/fale-conosco.component.ts
+++ src/app/fale-conosco/fale-conosco.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-fale-conosco',
   templateUrl: './fale-conosco.component.html',
   styleUrls: ['./fale-conosco.component.css']
 })
-export class FaleConoscoComponent implements OnInit {
+export class FaleConoscoComponent {
 
-  constructor() { }
+  @Input() name = '';
+  @Input() email = '';
+  @Input() subject = '';
+  @Input() message = '';
 
-  ngOnInit() {
+  sendMessage(event: Event) {
+    event.preventDefault();
+    console.log(this);
   }
 
 }
