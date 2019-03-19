c6fa7207dbcc4339583af2c0b57bb1cb7642c37b
Marcelo Toyoda <marcelohtoy@gmail.com>
2019-03-19 02:47:40 -0300

    Ajuste de componente de consulta

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { KeyValue } from '../formulario-prestador/keyValue';
+import { HttpService } from '../service/http.service';
 
 @Component({
   selector: 'app-search',
@@ -8,11 +10,20 @@ import { Router } from '@angular/router';
 })
 export class SearchComponent implements OnInit {
 
+  atividades: KeyValue[] = [];
+  negocios: KeyValue[] = [];
+
   @Input() termEmpresa = '';
   @Input() termTipoNegocio = '';
   @Input() termPrestador = '';
 
-  constructor(private router: Router) { }
+  constructor(private http: HttpService, private router: Router) {
+    this.http.get('http://18.228.163.212:8080/api/atividade-profissional').subscribe(resposta =>
+      this.atividades = resposta);
+    
+    this.http.get('http://18.228.163.212:8080/api/tipo-negocio').subscribe(resposta =>
+      this.negocios = resposta);
+  }
 
   ngOnInit() {
   }
